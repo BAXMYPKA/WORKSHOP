814dca618e6a2e2f9da13379179f821a59de8300
Dmitriy <dmitriy.shumarov@x5.ru>
2019-10-02 17:54:39 +0300

    Thymeleaf templates

--- internal/src/main/java/workshop/internal/controllers/LoginController.java
+++ internal/src/main/java/workshop/internal/controllers/LoginController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.exceptions.TemplateProcessingException;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -26,13 +28,25 @@ public class LoginController {
 	@Value("${supportedLanguages}")
 	private String headerContentLanguageValue;
 	
+	@Value("${placeholder.adminPassword}")
+	private String placeholderAdminPassword;
+	
+	@Value("${placeholder.adminLogin}")
+	private String placeholderAdminLogin;
+	
 	@Autowired
 	private MessageSource messageSource;
 	
 	@GetMapping
 	public String getLogin(Model model, Locale locale) {
 		log.trace("Internal login entered with locale={}", locale.getDisplayLanguage());
+		Map<String, String> placeholders = new HashMap<>();
+		placeholders.put("adminLogin", placeholderAdminLogin);
+		placeholders.put("adminPassword", placeholderAdminPassword);
+		
 		model.addAttribute("supportedLanguages", headerContentLanguageValue.split(","));
+		model.addAttribute("placeholders", placeholders);
+		
 		return "login";
 	}
 	

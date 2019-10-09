a16a01dba88fa4d9292bdd71351b8d7a1e3a1c1c
Sersus <mrbaxmypka@gmail.com>
2019-10-09 18:37:44 +0300

    Design shot

--- external/src/main/java/workshop/controllers/external/IndexController.java
+++ external/src/main/java/workshop/controllers/external/IndexController.java
@@ -1,8 +1,9 @@
-package workshop.controllers;
+package workshop.controllers.external;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import workshop.controllers.WorkshopControllerAbstract;
 
 @Controller
 @RequestMapping(path = "/")

26001fca8179a8672edc4a1ee134c8bfb5c506b9
Dmitriy <dmitriy.shumarov@x5.ru>
2019-10-15 16:20:04 +0300

    Login html

--- internal/src/main/java/workshop/controllers/internal/InternalLoginController.java
+++ internal/src/main/java/workshop/controllers/internal/InternalLoginController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,8 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.thymeleaf.exceptions.TemplateProcessingException;
+import workshop.internal.entities.Employee;
+import workshop.internal.services.EmployeesService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -32,18 +36,24 @@ public class InternalLoginController {
 	private String placeholderAdminLogin; //Just for the presentation purposes
 	
 	@Autowired
+	private EmployeesService employeesService;
+	
+	@Autowired
 	private MessageSource messageSource;
 	
 	@GetMapping
 	public String getLogin(Model model, Locale locale) {
 		log.trace("Internal login entered with locale={}", locale.getDisplayLanguage());
+		
 		Map<String, String> placeholders = new HashMap<>();
 		placeholders.put("adminLogin", placeholderAdminLogin);
 		placeholders.put("adminPassword", placeholderAdminPassword);
 		
+		List<Employee> employees = employeesService.findAllEntities(0, 0, "lastName", Sort.Direction.ASC);
+		
 		model.addAttribute("supportedLanguages", headerContentLanguageValue.split(","));
 		model.addAttribute("placeholders", placeholders);
-		
+		model.addAttribute("employees", employees);
 		return "internal/login";
 	}
 	
